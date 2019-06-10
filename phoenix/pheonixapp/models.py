f66b1cc5e407f61c3d9a5c4ba93dd03af48f5913
BasmaAltayeh <basma.altayeh@gmail.com>
2019-06-10 11:30:41 +0300

    test

--- phoenix/pheonixapp/models.py
+++ phoenix/pheonixapp/models.py
@@ -14,6 +14,7 @@ class Blogs(models.Model):
         'User',
         on_delete=models.CASCADE,
     )
+    title = models.CharField(max_length=100,default='SOME STRING')
     Blog = models.TextField(blank=True)
 
 
