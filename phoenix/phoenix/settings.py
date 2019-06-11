b2977db4d457e9f2cee3d404c50c0fb3f9c092e6
areejobaid94 <rawan.akram27@yahoo.com>
2019-06-11 18:49:35 +0300

    sign in sign up

--- phoenix/phoenix/settings.py
+++ phoenix/phoenix/settings.py
@@ -76,27 +76,27 @@ WSGI_APPLICATION = 'phoenix.wsgi.application'
 # https://docs.djangoproject.com/en/2.2/ref/settings/#databases
 
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.mysql',
-#         'NAME': 'PhoenixDb',
-#         'USER':'root',
-#         'PASSWORD': '',
-#         'HOST':'localhost',
-#         'PORT':'3325',
-#     }
-# }
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'aaaa',
+        'NAME': 'PhoenixDb',
         'USER':'root',
         'PASSWORD': '',
         'HOST':'localhost',
         'PORT':'3325',
     }
 }
+
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.mysql',
+#         'NAME': 'aaaa',
+#         'USER':'root',
+#         'PASSWORD': '',
+#         'HOST':'localhost',
+#         'PORT':'3325',
+#     }
+# }
 
 
 
