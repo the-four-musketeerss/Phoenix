41c3315fe3a531d4be3fa299772820ae00d5dcf8
iman-elokour <elokour.iman@gmail.com>
2019-06-16 16:51:57 +0300

    fixed

--- client/src/signIn.js
+++ client/src/signIn.js
@@ -3,6 +3,20 @@ import './signIn.css';
 import { Route, Redirect } from 'react-router'
 import Mainprofile from './mainprofile.js';
 import { MDBContainer, MDBRow, MDBCol, MDBInput, MDBBtn } from 'mdbreact';
+/////////////////////////
+import Avatar from '@material-ui/core/Avatar';
+import Button from '@material-ui/core/Button';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import TextField from '@material-ui/core/TextField';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+import Link from '@material-ui/core/Link';
+import Paper from '@material-ui/core/Paper';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+import LockOutlinedIcon from '@material-ui/icons/LockOutlined';
+import Typography from '@material-ui/core/Typography';
+import { makeStyles } from '@material-ui/core/styles';
 
 class SignIn extends React.Component{
     constructor(props){
@@ -56,55 +70,91 @@ class SignIn extends React.Component{
     }
   
      render(){
-      return(<div id="div">
-         {!this.state.toggleSignIn ? (
-          <MDBContainer>
-      <MDBRow>
-        <MDBCol md="6">
-          <form>
-            <p className="h5 text-center mb-4">Sign in</p>
-            <div className="grey-text">
-              <MDBInput
-               onChange ={this.yourdata.bind(this)}
-                name = "email"
-                label="Type your email"
-                icon="envelope"
-                group
-                type="email"
-                validate
-                error="wrong"
-                success="right"
-              />
-              <MDBInput
-                name = "password"
-                onChange ={this.yourdata.bind(this)}
-                label="Type your password"
-                icon="lock"
-                group
-                type="password"
-                validate
-              />
+      return(
+        <Grid container component="main" style={{height: '100vh'}}>
+          <Grid item xs={false} sm={4} md={7} style={{backgroundImage:"url('https://source.unsplash.com/random')" ,backgroundRepeat:"no-repeat",backgroundSize:"cover", backgroundPosition: 'center'}} />
+          <Grid item xs={12} sm={8} md={5} component={Paper} elevation={6} square>
+
+
+
+
+            <div id="div" style={{margin:"theme.spacing(8, 4)",display: 'flex',flexDirection: 'column',alignItems: 'center'}}>
+              <Avatar style={{ margin:"theme.spacing(1)",backgroundColor:"theme.palette.secondary.main"}}>
+                <LockOutlinedIcon />
+              </Avatar>
+              <Typography component="h1" variant="h5">
+                Sign in
+              </Typography>
+              <form style={{ width: '100%', marginTop:'theme.spacing(1)'}} noValidate>
+              {!this.state.toggleSignIn ? (
+                     <div>
+                       <TextField
+                          onChange ={this.yourdata.bind(this)}
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          id="email"
+                          label="Email Address"
+                          name="email"
+                          autoComplete="email"
+                          autoFocus
+                        />
+                    {/* <input
+                      onChange ={this.yourdata.bind(this)}
+                      name = "email"
+                      placeholder=  " your email"
+                      type="email"
+                    /> */}
+
+                        <TextField
+                          onChange ={this.yourdata.bind(this)}
+                          variant="outlined"
+                          margin="normal"
+                          required
+                          fullWidth
+                          name="password"
+                          label="Password"
+                          type="password"
+                          id="password"
+                          autoComplete="current-password"
+                        />
+                         <FormControlLabel
+                            control={<Checkbox value="remember" color="primary" />}
+                            label="Remember me"
+                        />
+                        <Button
+                          onClick={this.server.bind(this)}
+                          type="submit"
+                          fullWidth
+                          variant="contained"
+                          color="primary"
+                         style={{ margin: 'theme.spacing(3, 0, 2)'}}
+                        >
+                          Sign In
+                        </Button>
+                        <Grid container>
+                          <Grid item xs>
+                            <Link href="#" variant="body2">
+                              Forgot password?
+                            </Link>
+                          </Grid>
+                        </Grid>
+                    </div>
+              ) : (
+              <Mainprofile              
+                  username = {this.state.username} 
+                  email={this.state.email}
+                  bio={this.state.bio}
+                  url={this.state.url} 
+                  id={this.state.id} 
+                  Redirect to="/mainprofile"
+                />
+              )}
+              </form>
             </div>
-            <div className="text-center">
-              <MDBBtn
-              onClick={this.server.bind(this)}
-              >Login</MDBBtn>
-            </div>
-          </form>
-        </MDBCol>
-      </MDBRow>
-    </MDBContainer>
-        ) : (
-         <Mainprofile              
-            username = {this.state.username} 
-			    	email={this.state.email}
-            bio={this.state.bio}
-            url={this.state.url} 
-            id={this.state.id} 
-            Redirect to="/mainprofile"
-          />
-				)}
-			</div>
+        </Grid>
+      </Grid>
     )}
   }
   export default SignIn;
